5cf7640fbd61c31a08e5529a5caf5c3061ab38f9
polopo927 <polopo927@mail.ru>
2024-03-30 16:07:35 +0300

    фикс email символов

--- src/js/modules/checkEmailInputs.ts
+++ src/js/modules/checkEmailInputs.ts
@@ -2,7 +2,7 @@ export const checkEmailInputs = (selector: string) => {
 	const emailInputs = document.querySelectorAll<HTMLInputElement>(selector)
 
 	emailInputs.forEach(input => {
-		const engKeyboard = /[^a-z 0-9 @]/ig
+		const engKeyboard = /[^a-z 0-9 @ . \- _ +]/ig
 		input.addEventListener('keypress', (event: KeyboardEvent) => {
 			if (event.key.match(engKeyboard)) {
 				event.preventDefault()
