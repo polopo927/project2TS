c56b0bc4fdcb6db0847a07ddadd8dabf9c85f57a
polopo927 <polopo927@mail.ru>
2024-03-30 12:44:18 +0300

    пояснения для себя

--- src/js/modules/forms.ts
+++ src/js/modules/forms.ts
@@ -1,7 +1,11 @@
 
+//функция отправки форм
 export const formsFeedback = () => {
+	//получаем все формы на странице
 	const forms = document.querySelectorAll('form')
+	//получаем все поля ввода на странице
 	const inputs = document.querySelectorAll('input')
+	//получаем поля ввода с name upload
 	const inputsUpload = document.querySelectorAll<HTMLInputElement>('[name="upload"]')
 
 	const messageFromSend = {
@@ -13,6 +17,7 @@ export const formsFeedback = () => {
 		fail: '/src/assets/img/fail.png'
 	}
 
+	//создаём асинхронную функцию отправки данных пользователем
 	const postData = async (url: string, data: string) => {
 		const result = await fetch(url, {
 			method: 'POST',
@@ -22,6 +27,7 @@ export const formsFeedback = () => {
 		return await result.text()
 	}
 
+	//функция очистки инпутов 
 	const clearInputs = () => {
 		inputs.forEach(input => {
 			input.value = ''
@@ -33,41 +39,63 @@ export const formsFeedback = () => {
 		})
 	}
 
+	//функция отображения загруженного пользователем файла
 	inputsUpload.forEach(input => {
 		input.addEventListener('input', () => {
 			if (input.files) {
+				//с помощью деструктуризации получаем имя и разрешение файла 
 				const [name, ext] = input.files[0].name.split('.')
+				//тернарное условие если имя файла длинее 10 символов, то dots присваиваем ..., если нет, то точку
 				const dots = name.length > 9 ? '...' : '.'
-				const fullName = name.substring(0, 6) + dots + ext
+				//получаем полное имя файла подставляем из переменной name символы с 0 по 9, если больше там больше дотс будет ... и разрешение файла
+				const fullName = name.substring(0, 9) + dots + ext
+				//если предыдущий элемент существует, он не является null
 				if (input.previousElementSibling) {
+					//то заменяем ему текст на полное имя файла 
 					input.previousElementSibling.textContent = fullName
 				}
 			}
 		})
 	})
 
+	//перебираем все формы
 	forms.forEach(form => {
+		//вешаем обработчик события на отправку формы
 		form.addEventListener('submit', event => {
+			//отключаем обычную работу бразуера, обновление страницы
 			event.preventDefault()
 
+			//создаём переменную в которую помещаем новый элемент div
 			const statusMessage = document.createElement('div')
+			//его стилям присваеваем селектор status(какая-то стилизация)
 			statusMessage.classList.add('status')
+			//и добавляем этот див в конец родителя
 			form.parentNode?.appendChild(statusMessage)
 
+			//добавляем форме стили анимации и исчезновения
 			form.classList.add('animated', 'fadeOutUp')
+			//и ставим таймер на переключение формы дисплея в none
 			setTimeout(() => {
 				form.style.display = 'none'
 			}, 400)
 
+			//создаём переменную и помещаем в неё новый элемент с тэгом img
 			const statusImg = document.createElement('img')
+			//получаем атрибут src у этого элемента и присваеваем ему путь который у нас хранится в переменной гифки спинера
 			statusImg.setAttribute('src', messageFromSend.spinner)
+			//добавляем элементу стили анимации и появления
 			statusImg.classList.add('animated', 'fadeInUp')
+			//пушим в наш верхний див новую переменную с картинкой
 			statusMessage.appendChild(statusImg)
 
+			//создаём переменную в которую помещаем новый элемент div 
 			const statusText = document.createElement('div')
+			//меняем её текст на сообщение загрузки
 			statusText.textContent = messageFromSend.loading
+			//и тоже пушим в наш первый новый див
 			statusMessage.appendChild(statusText)
 
+			//создаём новую форму
 			const formData = new FormData(form)
 
 			const jsonObject: { [key: string]: FormDataEntryValue } = {};
@@ -81,7 +109,6 @@ export const formsFeedback = () => {
 				}
 			});
 			const jsonData = JSON.stringify(jsonObject);
-			console.log(jsonData)
 
 			const url = 'https://simple-server-2ow5.onrender.com/api/data'
 			postData(url, jsonData)
